e6b42ad3a0e60cceb5ffd6357858c0071a1151c8
liweichao <liweichao@gmail.com>
2014-04-25 15:48:05 +0800

    add signup fuction

--- NetTester/src/edu/nettester/SignupActivity.java
+++ NetTester/src/edu/nettester/SignupActivity.java
@@ -1,16 +1,27 @@
 package edu.nettester;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import edu.nettester.task.OPHTTPClient;
+import edu.nettester.util.CommonMethod;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends ActionBarActivity {
     
@@ -43,8 +54,53 @@ public class SignupActivity extends ActionBarActivity {
         btn_signup.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+            	String str_email = edit_email.getText().toString();
+                String str_pwd1 = edit_pwd1.getText().toString();
+                String str_pwd2 = edit_pwd2.getText().toString();
+                
+                if (str_email.isEmpty() || str_pwd1.isEmpty() || str_pwd2.isEmpty()) {
+                    Toast.makeText(SignupActivity.this, "Email or password should be not null", Toast.LENGTH_SHORT)
+                         .show();
+                } else if(!str_pwd1.equals(str_pwd2)) {
+                	Toast.makeText(SignupActivity.this, "The two passwords should be the same", Toast.LENGTH_SHORT)
+                    .show();
+                } else {
+                    if (sendSignupData(str_email, str_pwd1)) {
+                    	
+                        // TODO login success
+                    }
+                }
             }
         });
+    }
+    
+    public boolean sendSignupData(String str_email, String str_pwd) {
+    	try {
+    		RegProc msignup = new RegProc();
+        	String fout = msignup.execute(str_email, str_pwd).get();
+        	
+        	if(fout.equals("exists")) {
+        		Toast.makeText(SignupActivity.this, "The user has already existed", Toast.LENGTH_SHORT)
+                .show();
+        	} else if(fout.equals("error")) {
+        		Toast.makeText(SignupActivity.this, "An error occurs", Toast.LENGTH_SHORT)
+                .show();
+        	} else {
+        		String[] outs = fout.split("\t");
+        		CommonMethod.M_UID = outs[0];
+        		CommonMethod.M_UNAME = outs[1];
+        		CommonMethod.M_HASH = outs[2];
+        		
+        		//redirect to the main page?
+        		
+        		
+        	}
+        	
+    	} catch (Exception e) {  
+            Log.e(CommonMethod.TAG, e.getMessage());
+        }
+    	
+        return false;
     }
     
     @Override
@@ -74,5 +130,31 @@ public class SignupActivity extends ActionBarActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    
+    private class RegProc extends AsyncTask<String, Void, String> {
+    	@Override
+		protected String doInBackground(String... params) {
+    		String output = "";
+    		List<NameValuePair> DataList = new ArrayList<NameValuePair>();
+        	
+        	DataList.add(new BasicNameValuePair("UserName", params[0]));
+        	DataList.add(new BasicNameValuePair("Password", params[1]));
+        	
+        	OPHTTPClient mclient = new OPHTTPClient();
+        	output = mclient.postPage(CommonMethod.reg_url, DataList);
+        	
+        	if (CommonMethod.DEBUG)
+        		Log.d(CommonMethod.TAG, output);
+        	
+        	mclient.destroy();
+        	
+    		return output;
+    	}
+    	
+    	@Override
+        protected void onPostExecute(String result) {
+    		
+    	}
     }
 }
