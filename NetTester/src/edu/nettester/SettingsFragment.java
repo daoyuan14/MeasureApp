74f2465135b0752717294ce6fcbd58b38acd963b
daoyuan14 <daoyuan0x@gmail.com>
2014-04-26 00:37:08 +0800

    update for last sync time

--- NetTester/src/edu/nettester/SettingsFragment.java
+++ NetTester/src/edu/nettester/SettingsFragment.java
@@ -12,6 +12,8 @@ public class SettingsFragment extends PreferenceFragment implements Constant {
     
     public static final String KEY_PREF_USERNAME = "pref_login_username";
     
+    public static final String KEY_PREF_LASTSERVER = "pref_server_last";
+    
     private SharedPreferences.OnSharedPreferenceChangeListener listener;
     
     @Override
@@ -29,11 +31,15 @@ public class SettingsFragment extends PreferenceFragment implements Constant {
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
                     String key) {
                 if (DEBUG)
-                    Log.d(TAG, "Enter onSharedPreferenceChanged");
+                    Log.d(TAG, "Enter onSharedPreferenceChanged "+key);
+                
+                Preference userPref = findPreference(key);
 
                 if (key.equals(KEY_PREF_USERNAME)) {
-                    Preference userPref = findPreference(key);
                     userPref.setSummary(sharedPreferences.getString(key, "Anonymous"));
+                }
+                else if (key.equals(KEY_PREF_LASTSERVER)) {
+                    userPref.setSummary(sharedPreferences.getString(key, "Never"));
                 }
             }
             
@@ -53,6 +59,9 @@ public class SettingsFragment extends PreferenceFragment implements Constant {
         SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
         Preference userPref = findPreference(KEY_PREF_USERNAME);
         userPref.setSummary(sharedPreferences.getString(KEY_PREF_USERNAME, "Anonymous"));
+        
+        userPref = findPreference(KEY_PREF_LASTSERVER);
+        userPref.setSummary(sharedPreferences.getString(KEY_PREF_LASTSERVER, "Never"));
     }
 
     @Override
--- NetTester/src/edu/nettester/task/SyncServerListTask.java
+++ NetTester/src/edu/nettester/task/SyncServerListTask.java
@@ -6,16 +6,21 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 
 import edu.nettester.R;
+import edu.nettester.SettingsFragment;
 import edu.nettester.util.CommonMethod;
 import edu.nettester.util.Constant;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 
 public class SyncServerListTask extends AsyncTask<Void, Void, Boolean> implements Constant {
     
@@ -42,6 +47,16 @@ public class SyncServerListTask extends AsyncTask<Void, Void, Boolean> implement
                 
                 // update server list
                 CommonMethod.readServerList(ServerListPath);
+                
+                // update preferences
+                long ts = System.currentTimeMillis();
+                SimpleDateFormat df = new SimpleDateFormat("MMM d HH:mm, yyyy");
+                String curtime = df.format(ts);
+                
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mView.getContext());
+                Editor editor = sharedPref.edit();
+                editor.putString(SettingsFragment.KEY_PREF_LASTSERVER, curtime);
+                editor.commit();
                 
                 result = true;
                 
