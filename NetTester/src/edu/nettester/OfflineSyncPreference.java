b8c215165b286f46d2e6270bacf03628b6224429
daoyuan14 <daoyuan0x@gmail.com>
2014-04-26 13:27:38 +0800

    add offline log sync preference

---
+++ NetTester/src/edu/nettester/OfflineSyncPreference.java
@@ -0,0 +1,20 @@
+package edu.nettester;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+
+public class OfflineSyncPreference extends Preference {
+
+    public OfflineSyncPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onClick() {
+        super.onClick();
+        
+        // TODO
+    }
+
+}
