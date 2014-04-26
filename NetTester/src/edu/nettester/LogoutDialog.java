68d2457e2e586a9d245d7aead98407f085dd1e0a
daoyuan14 <daoyuan0x@gmail.com>
2014-04-26 23:32:35 +0800

    delete more info when log out

--- NetTester/src/edu/nettester/LogoutDialog.java
+++ NetTester/src/edu/nettester/LogoutDialog.java
@@ -1,5 +1,8 @@
 package edu.nettester;
 
+import edu.nettester.util.CommonMethod;
+import edu.nettester.util.Constant;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -7,7 +10,7 @@ import android.preference.DialogPreference;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 
-public class LogoutDialog extends DialogPreference {
+public class LogoutDialog extends DialogPreference implements Constant {
     
     private Context mContext;
 
@@ -27,7 +30,13 @@ public class LogoutDialog extends DialogPreference {
             SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
             Editor editor = sharedPref.edit();
             editor.remove(SettingsFragment.KEY_PREF_USERNAME);
+            editor.remove(PREF_MUID);
+            editor.remove(PREF_MHASH);
             editor.commit();
+            
+            CommonMethod.M_UID = "0";
+            CommonMethod.M_UNAME = "Anonymous";
+            CommonMethod.M_HASH = "";
         }
     }
 
