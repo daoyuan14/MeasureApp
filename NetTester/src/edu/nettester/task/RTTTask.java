99f9d4c2bc8b73585be5c5a41f42a5b71e2c84c5
daoyuan14 <daoyuan0x@gmail.com>
2014-04-27 00:48:06 +0800

    progress bar for Upload, change the loop from 1 to 7

--- NetTester/src/edu/nettester/task/RTTTask.java
+++ NetTester/src/edu/nettester/task/RTTTask.java
@@ -12,7 +12,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpConnectionParams;
 
@@ -417,6 +419,123 @@ public class RTTTask extends AsyncTask<String, Integer, String[]> implements Con
             }
             
             return tp;
+        }
+    }
+    
+    public class HTTPUpTP implements Constant {
+        private String mserver = "";
+        private String OUTAG = "NetTester";
+        
+        public HTTPUpTP(String in_server) {
+            this.mserver = in_server;
+        }
+        
+        public float execute() {
+            float tp = 0;
+            boolean success = false;
+            
+            try {
+                String up_url = this.mserver+"upload.php";
+                AndroidHttpClient client = AndroidHttpClient.newInstance("Dalvik/1.6.0 NetTester of OneProbe Group");
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 3000);
+                String p = prepare_str(250000);
+                
+                for(int i=0;i<7;i++) {
+                    int statusCode = 0;
+                    //long ts = System.nanoTime();
+                    long upsize = p.length();
+                    
+                    ArrayList<NameValuePair> DataList = new ArrayList<NameValuePair>();
+                    
+                    DataList.add(new BasicNameValuePair("data", p));
+                    
+                    HttpPost httppost = new HttpPost(up_url);
+                    httppost.setEntity(new UrlEncodedFormEntity(DataList));
+                    
+                    long startTime = System.nanoTime();
+                    
+                    HttpResponse response = client.execute(httppost);
+                    
+                    StatusLine statusLine = response.getStatusLine();
+                    if (statusLine != null) {
+                        statusCode = statusLine.getStatusCode();
+                        success = (statusCode == 200);
+                    }
+                    
+                    if(success) {
+                        try{
+                            HttpEntity responseEntity = response.getEntity();
+                            
+                            BufferedInputStream bufferedinStrm;
+                            bufferedinStrm = new BufferedInputStream(responseEntity.getContent());                      
+                            
+                            // TO-DO  getContentLength() returns negative number if body length is unknown
+                            //long downsize = responseEntity.getContentLength();
+                            
+                            byte abyte[];
+                            abyte = new byte[32768];                        
+                            long have_read = 0;
+                            long readLen;
+                            
+                            while ((readLen = bufferedinStrm.read(abyte)) > 0) {
+                                have_read += readLen;
+                            }
+                            
+                            long endTime = System.nanoTime();
+                            
+                            float duration = (endTime-startTime)/1000000;
+                            if (DEBUG)
+                                Log.d(TAG, String.valueOf(upsize)+":"+String.valueOf(have_read)+":"+String.valueOf(duration));
+                            tp = (upsize*8)/duration;
+                            
+                            if(duration > 8000) {
+                                break;
+                            }
+                            TimeUnit.MILLISECONDS.sleep(200);
+                        } catch (InterruptedException e){
+                            Log.e(TAG, e.getMessage());
+                        }
+                    } else {
+                        Log.w(TAG, "download failed");
+                    }
+                    httppost.abort();
+                    
+                    //p = generateNewStr(p, 2^(i+1));
+                    p = generateNewStr(p, 2);
+                    
+                    publishProgress(new Integer[] {Cat_UPLOAD, i*(100/7)});
+                }
+                
+                client.close();
+            } catch (Exception e) {  
+                Log.e(OUTAG, e.getMessage());
+            }
+            return tp;
+        }
+        
+        private String prepare_str(int strLen)
+        {
+            StringBuilder stringbuilder;
+            for (stringbuilder = new StringBuilder(strLen);stringbuilder.length() < strLen;stringbuilder.append((long)(1.0D + Math.floor(100000000D * Math.random())))) {
+                
+            }
+            return stringbuilder.toString();
+        }
+        
+        /**
+         * 
+         * @param basestr
+         * @param num Multiple number
+         * @return
+         */
+        private String generateNewStr(String basestr, int num) {
+            int strlen = num * basestr.length();
+            StringBuilder sb = new StringBuilder(strlen);
+            
+            for (int i = 0; i < num; i++)
+                sb.append(basestr);
+            
+            return sb.toString();
         }
     }
     
